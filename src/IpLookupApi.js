70d8a365862aa73936990549c425d239ca80309f
Caro Evans <caroline.evans@pieronline.org>
2021-08-29 15:14:24 +1000

    adding todos for testing

--- src/IpLookupApi.js
+++ src/IpLookupApi.js
@@ -1,5 +1,7 @@
 const IpLookupApi = {
 
+  // TO DO: test these methods
+
   findLocationData() {
     return fetch('https://extreme-ip-lookup.com/json/')
     .then( response => response.json())
--- src/WeatherApi.js
+++ src/WeatherApi.js
@@ -3,6 +3,8 @@ class WeatherApi {
     this.apiKey = apiKey
   }
 
+  // TO DO: test these methods
+
   findWeatherData = (location_string) => {
     return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location_string}&units=metric&appid=${this.apiKey}`)
     .then(response => response.json())
